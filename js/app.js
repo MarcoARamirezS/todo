d5239b97aa2a61edaed6dceef70f9da266cf33fc
Marco Ramirez <marco@functionize.com>
2022-08-26 10:59:08 -0500

    add btnActionsÃ

--- js/app.js
+++ js/app.js
@@ -15,6 +15,11 @@ document.addEventListener('DOMContentLoaded', () => {
     pintarTareas()
 })
 
+listaTareas.addEventListener('click', (event) => {
+    // console.log(event)
+    btnAcciones(event)
+})
+
 formulario.addEventListener('submit', e => {
     e.preventDefault()
     // console.log('evento', e)
@@ -35,6 +40,7 @@ const setTarea = e => {
         estado: false
     }
     tareas[tarea.id] = tarea
+    // console.log(tareas)
     pintarTareas()
     formulario.reset()
     e.target.querySelector('input').focus()
@@ -67,4 +73,25 @@ const pintarTareas = () => {
         fragment.appendChild(clone)
     })
     listaTareas.appendChild(fragment)
+}
+
+const btnAcciones = (click) => {
+    // console.log(click)
+    if(click.target.classList.contains('fa-check-circle')){
+        tareas[click.target.dataset.id].estado = true
+        pintarTareas()
+    }
+
+    if(click.target.classList.contains('fa-minus-circle')){
+        delete tareas[click.target.dataset.id]
+        pintarTareas()
+    }
+
+    if(click.target.classList.contains('fa-undo-alt')){
+        tareas[click.target.dataset.id].estado = false
+        pintarTareas()
+    }
+
+    click.stopPropagation();
+    
 }
